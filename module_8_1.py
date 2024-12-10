8e99ef81d1d832e1ec141afcb1e8e08239df7f73
LyovkinIlya <lyovkin2001@mail.ru>
2024-12-10 11:20:24 +0300

    10.12

--- module_8_1.py
+++ module_8_1.py
@@ -3,7 +3,7 @@ def add_everything_up(a, b):
         ab = a + b
     except TypeError:
         return str(a) + str(b)
-    return '{:.2f}'.format(ab)
+    return '{:.3f}'.format(ab)
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
